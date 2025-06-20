4ad5f7d8e165e5b34cc95665beb9b0ad50103917
Ariel Otilibili <otilibil@eurecom.fr>
2025-06-20 17:23:54 +0200

    wip: refactor, change function signatures
    Strings coming from Python are null-terminated. With that, functions and
    signatures are refactored.
    
    Signed-off-by: Ariel Otilibili <otilibil@eurecom.fr>

--- src/string.zig
+++ src/string.zig
@@ -2,32 +2,23 @@
 //! you are making an executable, the convention is to delete this file and
 //! start with main.zig instead.
 const std = @import("std");
-const testing = std.testing;
+const mem = std.mem;
 
-pub export fn contains(haystack: [*]const u8, needle: [*]const u8) bool {
-    const haystack_nt: [*:0]const u8 = @ptrCast(haystack);
-    const needle_nt: [*:0]const u8 = @ptrCast(needle);
-
-    const haystack_slice: []const u8 = std.mem.span(haystack_nt);
-    const needle_slice: []const u8 = std.mem.span(needle_nt);
-
-    return if (std.mem.indexOf(u8, haystack_slice, needle_slice) != null) true else false;
+pub export fn contains(haystack: [*:0]const u8, needle: [*:0]const u8) bool {
+    return if (mem.indexOf(u8, mem.span(haystack), mem.span(needle)) == null) false else true;
 }
 
-pub export fn extract_member(string: [*]const u8) [*]const u8 {
+pub export fn extract_member(string: [*:0]const u8) [*]const u8 {
     return extract_element(string, "set member ");
 }
 
-pub export fn extract_subnet(string: [*]const u8) [*]const u8 {
+pub export fn extract_subnet(string: [*:0]const u8) [*]const u8 {
     return extract_element(string, "set subnet ");
 }
 
-fn extract_element(string: [*]const u8, marker: []const u8) [*]const u8 {
-    const string_nt: [*:0]const u8 = @ptrCast(string);
-    const string_slice: []const u8 = std.mem.span(string_nt);
+fn extract_element(string: [*:0]const u8, marker: []const u8) [*]const u8 {
+    const trimmed = mem.trimLeft(u8, mem.span(string), " \t");
+    const index = mem.indexOf(u8, marker, trimmed).? + marker.len;
 
-    const trimmed_slice = std.mem.trimLeft(u8, string_slice, " \t");
-    const index = std.mem.indexOf(u8, marker, trimmed_slice).? + marker.len;
-
-    return trimmed_slice[index..].ptr;
+    return trimmed[index..].ptr;
 }
